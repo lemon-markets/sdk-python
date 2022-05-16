03968c0ece04769891b0310c00b54e13ce158ddb
ihor rudynskyi <ihor.rudynskyi@gmail.com>
2022-05-16 17:05:53 +0200

    fix review findings

--- setup.py
+++ setup.py
@@ -1,21 +1,20 @@
+import os
 from distutils.core import setup
 
 from pkg_resources import parse_requirements
 from setuptools import find_packages
 
+version_content = {}
+with open(os.path.join("lemon", "version.py"), encoding="utf-8") as f:
+    exec(f.read(), version_content)
 
-def get_version():
-    with open("lemon/__init__.py") as file:
-        for line in file:
-            if line.startswith("version"):
-                return line.split('"')[1]
-
+version = version_content["VERSION"]
 
 setup(
     name="lemon",
-    version=get_version(),
-    description="Official python sdk for lemon.markets",
-    long_description=open("README.md").read(),
+    version=version,
+    description="Official Python client for lemon.markets API",
+    long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
     license="MIT",
     classifiers=[
@@ -32,12 +31,13 @@ setup(
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
     ],
-    # author="",
-    # author_email="",
+    author="lemon.markets",
+    author_email="support@lemon.markets",
     url="https://github.com/lemon-markets/sdk-python/",
     download_url="https://pypi.python.org/pypi/lemon/",
     project_urls={
         "Source": "https://github.com/lemon-markets/sdk-python/",
+        "Changes": "https://github.com/lemon-markets/sdk-python/blob/master/CHANGELOG.md",
         "Tracker": "https://github.com/lemon-markets/sdk-python/issues/",
         "lemon.markets": "https://www.lemon.markets/",
         "API Documentation": "https://docs.lemon.markets/",
