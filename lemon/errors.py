3b7c2c4b0a6436e0600032d6d7ee5a634f8a63f2
ihor-rud <31574816+ihor-rud@users.noreply.github.com>
2022-06-13 17:43:59 +0200

    extend error handling (#37)
    * extend error handling
    
    * refactor exceptions
    
    * refactor 2 tests
    
    * reimplement errors
    
    * changes
    
    * fix review findings
    
    Co-authored-by: Pawel Dynowski <pawel.dynowski@gmail.com>

--- lemon/errors.py
+++ lemon/errors.py
@@ -1,43 +1,64 @@
-import enum
 from datetime import datetime
-from typing import Dict, Type, TypeVar
-
-TApiError = TypeVar("TApiError", bound="ApiError")
+from typing import Any, Dict
 
 
-class ErrorCodes(str, enum.Enum):
-    UNAUTHORIZED = "unauthorized"
-    INTERNAL_ERROR = "internal_error"
-    INVALID_QUERY = "invalid_query"
+class BaseLemonError(Exception):
+    """Base class for all lemon.markets errors."""
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}()"
 
 
-class BaseError(Exception):
-    pass
-
-
-class ApiError(BaseError):
-    def __init__(self, time: datetime, error_code: ErrorCodes, error_message: str):
+class LemonError(BaseLemonError):
+    def __init__(self, time: datetime, error_code: str, error_message: str):
         super().__init__(time, error_code, error_message)
         self.time = time
         self.error_code = error_code
         self.error_message = error_message
 
     @classmethod
-    def _from_data(cls: Type[TApiError], data: Dict[str, str]) -> TApiError:
+    def _from_data(cls, data: Dict[str, str]) -> "LemonError":
         return cls(
             time=datetime.fromisoformat(data["time"]),
-            error_code=ErrorCodes(data["error_code"]),
+            error_code=data["error_code"],
             error_message=data["error_message"],
         )
 
+    def __repr__(self) -> str:
+        return (
+            f"{self.__class__.__name__}"
+            f"("
+            f"time={self.time.isoformat()}, "
+            f"error_code={self.error_code}, "
+            f"error_message={self.error_message}"
+            f")"
+        )
 
-class AuthenticationError(ApiError):
-    pass
+
+class InvalidRequestError(LemonError):
+    ...
 
 
-class InternalServerError(ApiError):
-    pass
+class AuthenticationError(LemonError):
+    ...
 
 
-class InvalidQueryError(ApiError):
-    pass
+class InternalServerError(LemonError):
+    ...
+
+
+class BusinessLogicError(LemonError):
+    ...
+
+
+class APIError(BaseLemonError):
+    def __init__(self, data: Any):
+        super().__init__(data)
+        self.data = data
+
+    @classmethod
+    def _from_data(cls, data: Dict[str, str]) -> "APIError":
+        return cls(data=data)
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}(data={self.data})"
--- tests/market_data/conftest.py
+++ tests/market_data/conftest.py
@@ -1,10 +1,21 @@
 import pytest
 from pytest_httpserver import HTTPServer
 
-from tests.conftest import CommonApiTests
+from lemon.api import Api
+from lemon.errors import InvalidRequestError
+from tests.conftest import CommonApiTests, build_error
 
 
 class CommonMarketDataApiTests(CommonApiTests):
     @pytest.fixture
     def httpserver(self, market_data_httpserver: HTTPServer):
         return market_data_httpserver
+
+    def test_handle_market_data_error(
+        self, client: Api, httpserver: HTTPServer, api_call_kwargs
+    ):
+        httpserver.expect_oneshot_request(**api_call_kwargs).respond_with_json(
+            build_error("invalid_request"), status=400
+        )
+        with pytest.raises(InvalidRequestError):
+            self.make_api_call(client)
