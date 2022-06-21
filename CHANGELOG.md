49c377ca88c645f307f2b48915e32dd094840205
Pawel Dynowski <pawel.dynowski@gmail.com>
2022-06-21 17:02:50 +0200

    update changelog and setup

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,37 +1,17 @@
-# 1.0.0
+# Changelog
 
-- Support those requests in the library:
+All notable changes to this project will be documented in this file.
 
-  - [Market Data API](https://docs.lemon.markets/market-data/overview)
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/).
 
-    - [GET /instruments/](https://docs.lemon.markets/market-data/instruments-tradingvenues#get-instruments)
-    - [GET /ohlc/{x1}/](https://docs.lemon.markets/market-data/historical-data#get-ohlcx1)
-    - [GET /quotes/latest/](https://docs.lemon.markets/market-data/historical-data#get-quoteslatest)
-    - [GET /trades/latest/](https://docs.lemon.markets/market-data/historical-data#get-tradeslatest)
-    - [GET /venues/](https://docs.lemon.markets/market-data/instruments-tradingvenues#get-venues)
+## [1.0.1] - 2022-06-21
+### Changed
+- include README.md and CHANGELOG.md in package description 
 
-  - [Trading API](https://docs.lemon.markets/trading/overview)
+### Fixed
+- fix license badge
+- relax the requirements
 
-    - [GET /account/](https://docs.lemon.markets/trading/account#get-account)
-    - [PUT /account/](https://docs.lemon.markets/trading/account#put-account)
-    - [GET /account/withdrawals/](https://docs.lemon.markets/trading/account#get-accountwithdrawals)
-    - [POST /account/withdrawals/](https://docs.lemon.markets/trading/account#post-accountwithdrawals)
-    - [GET /account/bankstatements/](https://docs.lemon.markets/trading/account#get-accountbankstatements)
-    - [GET /account/documents/](https://docs.lemon.markets/trading/account#get-accountdocuments)
-    - [PUT /account/documents/{document_id}/](https://docs.lemon.markets/trading/account#get-accountdocumentsdocument_id)
-    - [GET /orders/](https://docs.lemon.markets/trading/orders#get-orders)
-    - [POST /orders/](https://docs.lemon.markets/trading/orders#post-orders)
-    - [POST /orders/{order_id}/activate/](https://docs.lemon.markets/trading/orders#post-ordersorder_idactivate)
-    - [GET /orders/{order_id}/](https://docs.lemon.markets/trading/orders#get-ordersorder_id)
-    - [DELETE /orders/{order_id}/](https://docs.lemon.markets/trading/orders#delete-ordersorder_id)
-    - [GET /positions/](https://docs.lemon.markets/trading/positions#get-positions)
-    - [GET /positions/statements/](https://docs.lemon.markets/trading/positions#get-positionsstatements)
-    - [GET /positions/performance/](https://docs.lemon.markets/trading/positions#get-positionsperformance)
-    - GET /user/
-
-- Implement error handling
-- Allow changing default timeout
-- Allow changing default retry_count
-- Allow changing default retry_backoff_factor
-- Allow changing default pool_connections
-- Allow changing default pool_maxsize
+## [1.0.0]  - 2022-06-21
+### Added
+- initial commit providing base SDK functionality
--- setup.py
+++ setup.py
@@ -3,42 +3,64 @@ from distutils.core import setup
 
 from setuptools import find_packages
 
-version_content = {}
-with open(os.path.join("lemon", "version.py"), encoding="utf-8") as f:
-    exec(f.read(), version_content)
 
-version = version_content["VERSION"]
+def get_version():
+    content = {}
+    with open(os.path.join("lemon", "version.py"), encoding="utf-8") as f:
+        exec(f.read(), content)
+    return content["VERSION"]
 
-with open("requirements.txt", "r") as fh:
-    requirements = fh.read().splitlines()
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+def get_long_description():
+    with open("README.md", "r") as fh:
+        long_description = fh.read()
+
+    long_description += "\n\n"
+
+    with open("CHANGELOG.md", "r") as fh:
+        long_description += fh.read()
+
+    return long_description
+
+
+def get_requirements():
+    with open("requirements.txt", "r") as fh:
+        requirements = fh.read().splitlines()
+    return requirements
+
 
 setup(
     name="lemon",
-    version=version,
+    version=get_version(),
     description="Official Python client for lemon.markets API",
-    long_description=long_description,
+    long_description=get_long_description(),
     long_description_content_type="text/markdown",
+    author="lemon.markets",
+    author_email="support@lemon.markets",
+    url="https://github.com/lemon-markets/sdk-python/",
+    packages=find_packages(include=["lemon", "lemon.*"]),
+    include_package_data=True,
+    python_requires=">=3.7, <4",
+    install_requires=get_requirements(),
     license="MIT",
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
-        "Operating System :: MacOS :: MacOS X",
-        "Operating System :: Microsoft :: Windows",
-        "Operating System :: POSIX :: Linux",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Libraries",
     ],
-    author="lemon.markets",
-    author_email="support@lemon.markets",
-    url="https://github.com/lemon-markets/sdk-python/",
     download_url="https://pypi.python.org/pypi/lemon/",
     project_urls={
         "Source": "https://github.com/lemon-markets/sdk-python/",
@@ -47,7 +69,4 @@ setup(
         "lemon.markets": "https://www.lemon.markets/",
         "API Documentation": "https://docs.lemon.markets/",
     },
-    packages=find_packages(include=["lemon", "lemon.*"]),
-    install_requires=requirements,
-    python_requires=">=3.7",
 )
