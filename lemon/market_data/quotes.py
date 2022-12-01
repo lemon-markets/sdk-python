97d6656c9c855132b227b15f0a0c9d3403c171ac
Paweł Dynowski <pawel.dynowski@gmail.com>
2022-12-01 11:13:55 +0100

    feat: support for historical quotes

--- lemon/market_data/quotes.py
+++ lemon/market_data/quotes.py
@@ -1,9 +1,9 @@
 from datetime import datetime
-from typing import List, Optional
+from typing import List, Literal, Optional, Union
 
 from lemon.base import Client
 from lemon.market_data.model import GetQuotesResponse
-from lemon.types import Sorting
+from lemon.types import Days, Sorting
 
 
 class Quotes:
@@ -25,6 +25,39 @@ class Quotes:
             params={
                 "isin": isin,
                 "mic": mic,
+                "decimals": decimals,
+                "epoch": epoch,
+                "sorting": sorting,
+                "limit": limit,
+                "page": page,
+            },
+        )
+        return GetQuotesResponse._from_data(
+            data=resp.json(),
+            t_type=float if decimals else int,
+            k_type=int if epoch else datetime.fromisoformat,  # type: ignore
+            client=self._client,
+        )
+
+    def get(
+        self,
+        isin: List[str],
+        mic: Optional[str] = None,
+        from_: Optional[Union[datetime, Literal["latest"]]] = None,
+        to: Optional[Union[datetime, Days]] = None,
+        decimals: Optional[bool] = None,
+        epoch: Optional[bool] = None,
+        sorting: Optional[Sorting] = None,
+        limit: Optional[int] = None,
+        page: Optional[int] = None,
+    ):
+        resp = self._client.get(
+            "quotes",
+            params={
+                "isin": isin,
+                "mic": mic,
+                "from": from_,
+                "to": f"P{to}D" if isinstance(to, Days) else to,
                 "decimals": decimals,
                 "epoch": epoch,
                 "sorting": sorting,
